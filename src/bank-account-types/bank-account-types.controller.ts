d4d19ad5e87674b03a7e719269936fa315b41cb0
Younes <younes.ghoniem@gmail.com>
2022-04-14 09:53:36 +0200

    swagger2

--- src/bank-account-types/bank-account-types.controller.ts
+++ src/bank-account-types/bank-account-types.controller.ts
@@ -12,7 +12,18 @@ import {
 import { BankAccountTypesService } from './bank-account-types.service';
 import { CreateBankAccountTypeDto } from './dto/create-bank-account-type.dto';
 import { UpdateBankAccountTypeDto } from './dto/update-bank-account-type.dto';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { BankAccountTypeDto } from './dto/bank-account-type.dto';
 
+@ApiTags('BankAccountTypes')
 @Controller('bank-account-types')
 export class BankAccountTypesController {
   constructor(
@@ -20,6 +31,15 @@ export class BankAccountTypesController {
   ) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new type of bank accounts' })
+  @ApiCreatedResponse({
+    description: 'The Bank account type has successfully been created.',
+    type: BankAccountTypeDto,
+  })
+  @ApiResponse({
+    status: 304,
+    description: 'Unable to create a new bank account type.',
+  })
   async create(@Body() createBankAccountTypeDto: CreateBankAccountTypeDto) {
     const bankAccountType = await this.bankAccountTypesService.create(
       createBankAccountTypeDto,
@@ -29,11 +49,30 @@ export class BankAccountTypesController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Find all bank account types',
+  })
+  @ApiOkResponse({
+    description: 'All bank account types were found.',
+    type: BankAccountTypeDto,
+    isArray: true,
+  })
   findAll() {
     return this.bankAccountTypesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Returns a single bank account type',
+    description: 'Returns a specific bank account type found by id',
+  })
+  @ApiOkResponse({
+    description: 'Bank account type successfully returned',
+    type: BankAccountTypeDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'Bank account type not found',
+  })
   async findOne(@Param('id') id: number) {
     const bankAccountType = await this.bankAccountTypesService.findOne(+id);
     if (bankAccountType) return bankAccountType;
@@ -41,8 +80,27 @@ export class BankAccountTypesController {
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Updates a single bank account type',
+    description: 'Updates a specific bank account type found by id',
+  })
+  @ApiOkResponse({
+    description: 'The bank account type has successfully been updated.',
+    type: BankAccountTypeDto,
+  })
+  @ApiResponse({
+    status: 304,
+    description: 'Unable to update the bank account type.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Bank account type not found.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: "The bank account type's id",
+  })
   update(
-    @Param('id') id: number,
+    @Param() { id }: { id: number },
     @Body() updateBankAccountTypeDto: UpdateBankAccountTypeDto,
   ) {
     const bankAccountType = this.bankAccountTypesService.update(
@@ -54,6 +112,17 @@ export class BankAccountTypesController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'deletes a bank account type',
+    description: 'deletes a specific bank account type found by id',
+  })
+  @ApiOkResponse({
+    description: 'Bank account type successfully deleted',
+    type: BankAccountTypeDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'Bank account type not found',
+  })
   async remove(@Param('id') id: number) {
     const bankAccountType = await this.bankAccountTypesService.remove(id);
     if (bankAccountType) return bankAccountType;
